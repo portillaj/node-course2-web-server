00734b673b158ccc235963a9483b0d1cf29dfdd4
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-05-14 18:49:47 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to respond to server log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  //res.send("<h1>Hello Express!</h1>");
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    message: 'welcome to the home page'
+  });
+});//end route
+
+app.get("/about", (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    error: "Unable to fulfill this request"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening');
+});
